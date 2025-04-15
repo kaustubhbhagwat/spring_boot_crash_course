51420fb1a541b5abc0912056bb3d782d9a67b425
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2025-04-15 20:17:15 +0530

    implementing auth

--- src/main/kotlin/com/kaustubh/spring_boot_crash_course/database/model/User.kt
+++ src/main/kotlin/com/kaustubh/spring_boot_crash_course/database/model/User.kt
@@ -2,9 +2,10 @@ package com.kaustubh.spring_boot_crash_course.database.model
 
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
 
+@Document("users")
 data class User(
-    @Id val id: ObjectId,
-    val userName: String,
+    val hashedPassword: String,
     val email: String
 )
